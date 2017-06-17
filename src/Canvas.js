9405ead7d80989b4f5923a94eaae349a9293002c
EliasHaaralahti <haaralahti.elias@gmail.com>
2017-06-17 21:19:56 +0300

    fix canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,70 +1,66 @@
 import React from 'react';
-import './Canvas.js';
 
-class Pixel extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      color: 0xff0000
+let randColor = function() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+class Box extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            color: randColor(),
+            size: 12
+        }
     }
-  }
 
-  render() {
-    return (
-    <div style={{
-        display: "inline-block",
-        background: this.state.color,
-        height: this.props.height,
-        width: this.props.width,
-    }} onClick={()=> this.setState({color: 0xffff00})} />
-    )
-  }
+    render() {
+        return <div style={{
+            display: "inline-block",
+            background: this.state.color,
+            height: this.state.size,
+            width: this.state.size,
+        }} onClick={()=> this.setState({color: randColor()})}/>;
+    }
 }
 
 let Row = (props) => {
-  return (
-    <div style={{height: props.height, width: props.width}}>
-      { Array(props.count).fill().map((_, i) => {
-        return <Pixel height={props.height}
-                    width={props.width / props.count}
-                    key={i} />
-      })}
-    </div>
-  )
+    return <div style={{
+        height: props.height,
+        width: props.width
+    }}>{
+        Array(props.count).fill().map((_, i) => {
+            return <Box height={props.height}
+                        width={props.width / props.count}
+                        key={i} />;
+        })
+    }</div>
 }
 
 class Grid extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      x: 1,
-      y: 1
+    constructor(props) {
+        super(props);
+        this.state = {
+            x: 150,
+            y: 100
+        }
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <div style={{position: "fixed", padding: "6px"}}>
-          <input type="range" value={this.state.x} min={1} max={25} onChange={(e)=>this.setState({x: parseInt(e.target.value)})} />
-          <input type="range" value={this.state.y} min={1} max={25} onChange={(e)=>this.setState({y: parseInt(e.target.value)})} />
-        </div>
-
+    render() {
+        return (
         <div style={{
             display: "inline-block",
             height: this.props.height,
             width: this.props.width
-        }}> {
-          Array(this.state.y).fill().map((_, i) => {
-            return <Row height={this.props.height / this.state.y}
-                        width={this.props.width}
-                        count={this.state.x}
-                        key={i} />
-          })
-        } </div>
-      </div>
-    )
-  }
+        }}>{
+            Array(this.state.y).fill().map((_, i) => {
+                return <Row height={this.props.height / this.state.y}
+                            width={this.props.width}
+                            count={this.state.x}
+                            key={i} />;
+            })
+        }</div>
+      )
+    }
 }
 
 export default Grid;
