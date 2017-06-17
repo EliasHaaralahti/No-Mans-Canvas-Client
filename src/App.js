327bad7590c3531f9205cab34a19724af3a04659
EliasHaaralahti <haaralahti.elias@gmail.com>
2017-06-17 22:41:29 +0300

    properly pass props to child components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component{
   render(){
     return(
       <div>
-        <Grid/>
+        <Grid />
       </div>
     )
   }
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-let randColor = function() {
-    return '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-class Box extends React.Component {
-    constructor() {
-        super();
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            color: randColor(),
-            size: 12
+            color: "#ffffff",
         }
     }
 
@@ -17,48 +12,54 @@ class Box extends React.Component {
         return <div style={{
             display: "inline-block",
             background: this.state.color,
-            height: this.state.size,
-            width: this.state.size,
-        }} onClick={()=> this.setState({color: randColor()})}/>;
+            height: this.props.size,
+            width: this.props.size,
+        }} onClick={()=> this.setState({color: "#ffff00"})}/>;
     }
 }
 
 let Row = (props) => {
-    return <div style={{
-        height: props.height,
-        width: props.width
-    }}>{
-        Array(props.count).fill().map((_, i) => {
-            return <Box height={props.height}
-                        width={props.width / props.count}
-                        key={i} />;
-        })
-    }</div>
+    return (
+      <div style={{
+          height: props.height,
+          width: props.width
+      }}>{
+          Array(props.count).fill().map((_, i) => {
+              return <Pixel size={props.height}
+                            key={i} />;
+          })
+        }
+      </div>
+  )
 }
 
 class Grid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            x: 150,
-            y: 100
+            // -3 due to padding, not optimal solution
+            x: Math.floor(window.innerWidth / 12) - 3,
+            y: Math.floor(window.innerHeight / 12) - 3
         }
     }
 
     render() {
-        return (
+      return (
+        // also props here?
         <div style={{
             display: "inline-block",
-            height: this.props.height,
-            width: this.props.width
+            height: window.innerHeight,
+            width: window.innerWidth
         }}>{
             Array(this.state.y).fill().map((_, i) => {
-                return <Row height={this.props.height / this.state.y}
-                            width={this.props.width}
+                // TODO: Use props?
+                return <Row height={12} //This also defines pixel size
+                            width={window.innerWidth}
                             count={this.state.x}
                             key={i} />;
             })
-        }</div>
+          }
+        </div>
       )
     }
 }
