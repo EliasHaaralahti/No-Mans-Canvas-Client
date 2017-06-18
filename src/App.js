53f652ba75c5d0ec683ea3ac49ce1317d5bd7f7a
EliasHaaralahti <haaralahti.elias@gmail.com>
2017-06-18 20:45:47 +0300

    update websocket

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import { createStore } from 'redux'
 
 import './App.css'
 import AppReducer from './AppReducer';
-import * as actions from './AppActions';
+// import * as actions from './AppActions';
 import Grid from './Canvas';
 import ColorMenu from './ColorMenu';
 import MessageBox from './MessageBox';
 
-const url = 'ws://localhost:8080';
+const url = 'ws://localhost:8080/canvas';
+
+var msg = {
+  "requestType": "initialAuth"
+}
 
 try {
   var socket = new WebSocket(url)
   socket.onopen = function(e) {
     console.log("Websocket: Connected");
-    socket.send("New connection has been made")
+    socket.send(JSON.stringify(msg))
   }
   socket.onmessage = function(e) {
     const data = JSON.parse(e.data);
       console.log("Websocket data received, printing....")
-      console.log(JSON.stringify(e.data))
+      console.log(JSON.stringify(data))
   }
 } catch(exception) {
   console.log("Websocket: Unable to connect!")
