e8c70f69c26779a96a9938fd74c6ebc986ec6a5b
EliasHaaralahti <haaralahti.elias@gmail.com>
2017-06-18 16:00:04 +0300

    change project structure, add websocket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css'
 import Grid from './Canvas';
+import ColorMenu from './ColorMenu';
+import MessageBox from './MessageBox';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>
-        <Grid />
-      </div>
-    )
+try {
+  var socket = new WebSocket('http://localhost:8000')
+  socket.onopen = function(e) {
+    console.log("Websocket: Connected");
   }
+  socket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+      console.log("do something with data...")
+  }
+} catch(exception) {
+  console.log("Websocket: Unable to connect!")
+}
+
+let App = props => {
+  if(socket == null) {
+    // return <MessageBox message="No response from server!" warning="True" />
+  }
+  return (
+    <div>
+      <Grid/>
+      <ColorMenu/>
+    </div>
+  )
 }
 
 export default App
