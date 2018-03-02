c61d548808d304592859e19d7a8fdffcea93301d
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2018-03-02 00:59:42 +0000

    implement messageBox Ok button

--- src/MessageBox.js
+++ src/MessageBox.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './MessageBox.css';
+import { setMessageBoxVisibility } from './AppActions';
 
 // TODO: Use const instead of component
 class MessageBox extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  onConfirm() {
+    setMessageBoxVisibility(false)
   }
 
   render() {
@@ -14,7 +19,7 @@ class MessageBox extends React.Component {
     return (
         <div className={className}>
           { this.props.message }
-          <input type="button" className="ok-button" value="Ok" />
+          <input type="button" className="ok-button" value="ok" onClick={this.onConfirm} />
         </div>
     )
   }
