b3be58653187cfd73723696bb8c675f2ce7c8a10
Elias Haaralahti <haaralahti.elias@gmail.com>
2022-05-14 19:13:28 +0300

    Fix issue in checking if mouse inside canvas

--- src/Components/CanvasComponent/canvasUtils.js
+++ src/Components/CanvasComponent/canvasUtils.js
@@ -2,6 +2,6 @@
 // Assumes canvas starts at 0,0 and ends at canvasWidth, canvasHeight.
 export function isMouseInsideCanvas(mousePositionX, mousePositionY, canvasWidth, canvasHeight) {
     if (mousePositionX >= 0 && mousePositionY >= 0 &&
-        mousePositionX <= canvasWidth, mousePositionY <= canvasHeight) return true;
+        mousePositionX <= canvasWidth && mousePositionY <= canvasHeight) return true;
     return false;
 }
